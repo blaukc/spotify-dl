63651b02f24829141ff3139adbf59f0950ca32cf
blaukc <brandonlau1@hotmail.com>
2021-10-17 13:48:49 +0800

    added config.py

--- spotify-dl.py
+++ spotify-dl.py
@@ -1,21 +1,17 @@
 from get_functions import get_track_link, get_album_link, get_spotify_playlist, get_spotify_playlist_name
+import config
 import os
 import platform
 import pexpect
 import wexpect
 from tabulate import tabulate
 
-arl = '3dadffaa3ed08377420042cf0ede03f5f02fe54be0499fde032737b32b8c3632d08ddca191262e50a16ae8f001a6877141188a6692a8f2426d6af58036e74232e9502403c1c2db250326471544ffb97f8663fa79035e50b4425947e632d56ae0'
-token = 'BQD4-8K1iv4qBgWykm45vO5sa3miB4HyZXckhvZfyf-z-dT_Ymj6FsdUMa0BBbsdKrM9JSKR5lWey0J4uugBIIXkLqHH-nODr4-zTFfy5pU7SeNQ-S_kckRz-HDMi-_iTDpFUuoAN0Hz_7GissCZ13c3rLFt'
-current_dir = os.path.dirname(os.path.abspath(__file__))
-target_dir = os.path.join(current_dir, 'music')
-
 def downloadmusic(download_dir, deezer_url, arl):
 
     if platform.system() == 'Windows':  #spawns child using wepect/pexpect
-        child = wexpect.spawn('deemix -b flac -p ' + download_dir + ' ' + deezer_url)
+        child = wexpect.spawn('deemix -b flac -p ' + '\"' + download_dir + '\" ' + deezer_url)
     else:
-        child = pexpect.spawn('deemix -b flac -p ' + download_dir + ' ' + deezer_url)
+        child = pexpect.spawn('deemix -b flac -p ' + '\"' + download_dir + '\" ' + deezer_url)
 
     i = child.expect(['Paste here your arl:', 'Download URL got'], timeout=10)
     if i == 0:
@@ -36,7 +32,7 @@ def downloadmusic(download_dir, deezer_url, arl):
 print('Starting Music Downloader...')
 playlist = input('Enter Spotify Playlist URL: ')
 #Gets tracks from spotify playlist
-tracks = get_spotify_playlist(playlist, token)
+tracks = get_spotify_playlist(playlist, config.spotify_token)
 
 
 #Checks playlist
@@ -60,8 +56,8 @@ for track in tracks:
 
 
 #Create directory
-name = get_spotify_playlist_name(playlist, token)
-download_dir = os.path.join(target_dir, name)
+name = get_spotify_playlist_name(playlist, config.spotify_token)
+download_dir = os.path.join(config.download_dir, name)
 try:
     os.mkdir(download_dir)
 except FileExistsError:
@@ -75,7 +71,7 @@ success = 0
 for track in tracks:
     progress += 1
     try:
-        downloadmusic(download_dir, track[3], arl)
+        downloadmusic(download_dir, track[3], config.deezer_arl)
         print('Completed [' + str(progress) + '/' + str(len(tracks)) + ']: ' + track[0])
         success += 1
     except:
