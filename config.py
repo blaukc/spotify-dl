cd88680575caa7b0b11430097c6e5b3a47e39001
blaukc <brandonlau1@hotmail.com>
2021-10-18 18:48:00 +0800

    removed deezer_python_lib

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 deezer_arl = '3dadffaa3ed08377420042cf0ede03f5f02fe54be0499fde032737b32b8c3632d08ddca191262e50a16ae8f001a6877141188a6692a8f2426d6af58036e74232e9502403c1c2db250326471544ffb97f8663fa79035e50b4425947e632d56ae0'
 spotify_token = 'BQAYedrs6wUUUAc_YCVgiNld5Ocdubyk_rF3cwCFfBmHdMVWcTPmRsow5S0zbopnXn5Stcph_YSghRA6kMuABuTfJP91bwKeRISci_dIBGE_tkzwA7GYTSmqMDt4G0LlH-mYJ08kTV3-BQjOMhfHyCHGp60R'
-#make sure double backslash
-download_dir = 'C:\\Users\\Brandon\\Brandon\\beepboop\\py\\2021\\spotify_dl\\music'
+#make sure to put double backslash \\ if using \
+download_dir = 'C:/Users/Brandon/Brandon/beepboop/py/2021/spotify_dl/music'
 #128, 320, flac
 bitrate = 'flac'
 #time before deemix stops downloading
